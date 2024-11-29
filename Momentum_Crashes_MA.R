7939e038244b7772a260f6dc453a71316a9a1c0a
smitikovski <mirkosmit98@gmail.com>
2024-11-29 14:06:50 +0100

    Include XLS DAX Tickers

--- Momentum_Crashes_MA.R
+++ Momentum_Crashes_MA.R
@@ -4,3 +4,22 @@ library(rugarch)
 library(data.table)
 library(ggplot2)
 library(stargazer)
+library(readxl)
+
+### Pathway to data
+path_to_data = "/Users/mirkosmit/Documents/CAU/Master_Thesis/Data/"
+xls_tickers = "DAX_Ticker_ISIN.xlsx"
+
+### Read in relevant ticker names
+Stocks = read_xlsx(paste0(path_to_data,xls_tickers))
+
+### Get Stock data from Yahoo Finance API and store it
+
+
+
+
+
+### Read in the data used in Daniel & Moskowitz
+constituents = (,)
+
+
