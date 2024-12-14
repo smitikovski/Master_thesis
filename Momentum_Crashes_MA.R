a9102d492fcdcd11e73a83b8daedff8bf3479fe5
smitikovski <149321140+smitikovski@users.noreply.github.com>
2024-12-15 00:53:09 +0100

    Deletion of unnecessary code for cleansing of DAX Data and import

--- Momentum_Crashes_MA.R
+++ Momentum_Crashes_MA.R
@@ -20,66 +20,7 @@ Stocks = fread(paste0(path_to_data,raw_Dax_Data))
 
 FSE_Stocks = read_xlsx(paste0(path_to_data, raw_FSE_monthly))
 
-
-### Data cleansing
-## Dax Stocks
-colnames(Stocks)
-
-Stocknames = colnames(Stocks)
-Stocknames_1 = list()
-#### extract the Stocknames from the colnames
-Stocknames = Stocknames[1:length(Stocknames)]
-Stocknames = gsub(" - TOT RETURN IND","",Stocknames)
-Stocknames = gsub(" - EARNINGS PER SHR","",Stocknames)
-Stocknames = gsub(" (XET) ","",Stocknames)
-##Stocknames = gsub("\\(.*?)","",Stocknames)
-Stocknames = str_trim(Stocknames, side = "both")
-Stocknames = unique(Stocknames)
-Stocknames
-
-### Filter for relevant Data
-Stocks = as.data.table(Stocks)
-setindex(Stocks, ...1)
-rel_dax_data = Stocks[, ..Stocknames]
-setnames(rel_dax_data, old = c("...1"), new = c("Date"))
-rel_dax_data[, Date := date(Date)]
-
-Data_copy = copy(rel_dax_data)
-
-
-
-## Cleanse for the XETRA-Data provided in the export file from EIKON
-colnames(Data_copy)
-Data_copy = Data_copy[, c(79:length(colnames(Data_copy)) ) := NULL]
-colnames(Data_copy)
-
-Adidas = Data_copy[, "ADIDAS"]
-Adidas[, ADIDAS_XET := rel_dax_data[,"ADIDAS (XET)"]]
-Adidas[, Date := rel_dax_data[,"Date"]]
-## Capping the Data to the dates at which Stocks entered and left the DAX
-colnames(rel_dax_data)
-
-Cec = Data_copy[,"CECONOMY"]
-Cec[,Date :=  Data_copy[, "Date"] ]
-
-Stock_names = colnames(Data_copy)
-writexl::write_xlsx(as.data.frame(Stock_names), path = path_to_data)
-
 ### Data Cleansing of monthly FSE Stock Data
-
-colnames(FSE_Stocks)
-FSE_stocknames = colnames(FSE_Stocks)
-
-FSE_stocknames = gsub(" - TOT RETURN IND", "", FSE_stocknames)
-FSE_stocknames = gsub(" - EARNINGS PER SHR", "", FSE_stocknames)
-
-FSE_stocknames = unique(FSE_stocknames)
-FSE_stocknames = FSE_stocknames[!grepl("#ERROR", FSE_stocknames)]
-FSE_stocknames = sub("\\.\\.\\.\\..*$", "", FSE_stocknames)
-FSE_stocknames = sub("\\.\\.\\..*$", "", FSE_stocknames)
-FSE_stocknames = unique(FSE_stocknames)
-FSE_stocknames
-
 
 columns_to_keep = !grepl("#ERROR", colnames(FSE_Stocks))
 FSE_Stocks = FSE_Stocks[, ..columns_to_keep]
@@ -99,21 +40,18 @@ FSE_monthly_p = FSE_monthly_p[, ..price_columns_to_keep]
 unique_cols = !duplicated(colnames(FSE_monthly_p))
 
 FSE_monthly_p = FSE_monthly_p[, ..unique_cols]
+colnames(FSE_monthly_p)
+setkey(FSE_monthly_p, Date)
+setcolorder(FSE_monthly_p, sort(colnames(FSE_monthly_p)))
+columns_to_keep2 = !grepl("#ERROR", colnames(FSE_monthly_p))
+FSE_monthly_p = FSE_monthly_p[, ..columns_to_keep2]
+setkey(FSE_monthly_p, Date)
 
-price_columns_to_keep
 
 
-# Names of the stocks are now cleanly extracted
-# -> Extract relevant Data from Import data.table
+# Stocks with 
 
-valid_stocknames <- FSE_stocknames[!FSE_stocknames %in% colnames(FSE_Stocks)]
-valid_stocknames
 
-rel_FSE_Stocks = FSE_Stocks[, ..FSE_stocknames]
-
-FSE_Stocks[, "BAYWA"]
-
-colnames(FSE_Stocks)
 
 
 ### Momentum Portfolio
