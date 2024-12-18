e1f7ab8322a7bc4e548e8d8c478edbfc7fc47348
smitikovski <149321140+smitikovski@users.noreply.github.com>
2024-12-18 02:03:47 +0100

    Creation WML factor

--- Momentum_Crashes_MA.R
+++ Momentum_Crashes_MA.R
@@ -28,7 +28,6 @@ Stocks = fread(paste0(path_to_data,raw_Dax_Data))
 
 FSE_Stocks = read_xlsx(paste0(path_to_data, raw_FSE_monthly))
 
-
 # Import EURIBOR data and format it
 EURIBOR = read_xlsx(paste0(path_to_data, EURIBOR_name))
 setnames(EURIBOR, c("DATE", "RF_1M", "RF_3M"))
@@ -88,10 +87,72 @@ FSE_monthly_p_l[, Return_cs := frollsum(Return, 11)]
 ### Momentum Portfolio
 # Rank stocks according to their return and put them into deciles
 
+# Calculate deciles for each Date based on the Return column -> only rank non-NA returns 
+FSE_monthly_p_l[, Decile := fifelse(
+  !is.na(Return_cs),  
+  as.integer(cut(Return_cs,
+                 breaks = quantile(Return_cs, probs = seq(0, 1, 0.1), na.rm = TRUE),
+                 include.lowest = TRUE, labels = FALSE)),
+  NA_integer_), by = Date]
+
+FSE_monthly_p_l[, Date := ymd(Date)]
+FSE_monthly_p_l[ Date == "2019-05-03", .SD]
 
 
-# Create wide table again
+## Testing whether breaking up into deciles actually worked
+FSE_monthly_p_l[ Date == "2019-05-03" & !is.na(Decile), .N]
+FSE_monthly_p_l[ Date == "2019-05-03" & Decile == 1, .N]
+FSE_monthly_p_l[ Date == "2019-05-03" & Decile ==10, .(Return_cs, Decile)]
+FSE_monthly_p_l[ Date == "2019-05-03" & Decile ==1, .(Return_cs, Decile)]
+
+
+## Testing another date
+FSE_monthly_p_l[ Date == "2023-10-03" & !is.na(Decile), .N]
+FSE_monthly_p_l[ Date == "2023-10-03" & Decile == 1, .N]
+FSE_monthly_p_l[ Date == "2023-10-03" & Decile ==10, .(Return_cs, Decile)]
+FSE_monthly_p_l[ Date == "2023-10-03" & Decile ==1, .(Return_cs, Decile)]
+
+2022-10-03
+FSE_monthly_p_l[ Date == "2022-10-03" & Decile == 10, .N]
+
+688/77
+
+# Seems to work
+## Now calculate the return of the winners minus the return of the losers
+
+# Formation one-month after evaluating the deciles of the returns
+FSE_monthly_p_l[, PF_Formation := shift(Decile, n=1)]
+
+WML_Data = FSE_monthly_p_l[PF_Formation == 10, .(WINNER = mean(Return_cs, na.rm = T) ), by = Date]
+setorder(WML_Data, Date)
+LOSERS = FSE_monthly_p_l[PF_Formation == 1, .(LOSER = mean(Return_cs, na.rm = T)), by = Date]
+WML_Data = merge(WML_Data, LOSERS, by = "Date")
+WML_Data[, WML := WINNER - LOSER]
+
+WML_Data[LOSER == min(LOSER), ]
+
+WML_Data[, max(WINNER)]
+#1: 1988-11-03         NA    NA
+#2: 2014-02-03   72.43583    NA
+#3: 2016-01-03   83.98931    NA
+#4: 2019-05-03 1814.81072    NA
+#5: 2023-10-03  395.05834    NA
+
+FSE_monthly_p_l[Date == "2014-02-03" & PF_Formation == 1] sum()
+
+
+## Investigation of results
+## 2024-12-03
+
+
+
+
+
+
+# Create wide table again visual comparability
 FSE_monthly_p_w = dcast(FSE_monthly_p_l, Date~variable, value.var = c("Return_cs"))
+
+
 
 
 
